eec9f33dcd5b44c969b1fe617979e1bf79ed1627
Dongkyun Lee <dongkyl@amazon.com>
2021-01-31 13:13:20 +0900

    remove auth config for google

--- web/lib/interfaces/config.ts
+++ web/lib/interfaces/config.ts
@@ -8,16 +8,7 @@ export const AmplifyConfig = {
   },
 }
 
-export const Cognito = {
-  Domain: `https://${ns.toLowerCase()}.auth.ap-northeast-2.amazoncognito.com`,
-  RedirectUri: 'http://localhost:3000',
-  Scopes: ['email', 'openid', 'profile'].join(' '),
-}
-
 export const IdentityProvider = {
-  Google: {
-    Url: `${Cognito.Domain}/oauth2/authorize?identity_provider=Google&redirect_uri=${Cognito.RedirectUri}&response_type=TOKEN&client_id=${AmplifyConfig.Auth.userPoolWebClientId}&scope=${Cognito.Scopes}`,
-  },
   Kakao: {
     Token: ``, // TODO: replace with actual kakao client id
   }
